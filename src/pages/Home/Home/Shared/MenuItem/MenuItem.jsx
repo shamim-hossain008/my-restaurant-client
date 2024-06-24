017f59c3862bea17c40356dc525a0f316ef82374
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-06-24 21:16:10 +0800

    Show Popular Menu Items In The Home Page & fix problem

--- src/pages/Home/Home/Shared/MenuItem/MenuItem.jsx
+++ src/pages/Home/Home/Shared/MenuItem/MenuItem.jsx
@@ -1,10 +1,17 @@
 const MenuItem = ({ item }) => {
-  console.log(item, " from menu items");
-
   return (
-    <div>
-      <h1>{item?.name}</h1>
-      <img src={item?.image} alt="" />
+    <div className="flex space-x-2">
+      <img
+        style={{ borderRadius: "0 200px 200px 200px" }}
+        className="w-[100px]"
+        src={item?.image}
+        alt=""
+      />
+      <div>
+        <h2 className="uppercase">{item?.name}--------</h2>
+        <p>{item?.recipe}</p>
+      </div>
+      <p className="text-yellow-500 ">${item?.price}</p>
     </div>
   );
 };
--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -14,12 +14,12 @@ const PopularMenu = () => {
       });
   }, []);
 
-  console.log(menu, "all popular data");
+  // console.log(menu, "all popular data");
 
   return (
-    <section>
+    <section className="mb-12">
       <SectionTitle heading={"From Our Menu"} subHeading={"Popular Items"} />
-      <div>
+      <div className="grid md:grid-cols-2 gap-10">
         {menu.map((item) => (
           <MenuItem key={item._id} item={item} />
         ))}
