c6130781588b89a7becd01d18bb6e77a4b8e7d73
shamim-hossain008 <shamim.hossain3573@gmail.com>
2024-06-28 21:29:28 +0800

    Redirect User After Login and create private route

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import {
   LoadCanvasTemplate,
   loadCaptchaEnginge,
@@ -12,6 +12,11 @@ import { AuthContext } from "../../providers/AuthProviders";
 const Login = () => {
   const [disabled, setDisabled] = useState(true);
   const { signIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/";
+
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
@@ -42,6 +47,7 @@ const Login = () => {
     `,
         },
       });
+      navigate(from, { replace: true });
     });
   };
 
